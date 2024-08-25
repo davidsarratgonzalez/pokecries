38be6043beae52e518e74fd9870cc4a12590b1cd
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-08-25 15:03:11 +0200

    style: phone toasts

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './PokemonCard.css';
 
 function PokemonCard({ pokemon, onClick, isAnimating, isCorrect, isVisible }) {
-  const feedbackClass = isAnimating ? (isCorrect ? 'correct' : 'incorrect') : '';
+  const [animationClass, setAnimationClass] = useState('');
+
+  useEffect(() => {
+    if (isAnimating) {
+      setAnimationClass(isCorrect ? 'correct-animation' : 'incorrect-animation');
+      const timer = setTimeout(() => {
+        setAnimationClass('');
+      }, 1000); // Duración de la animación aumentada a 1 segundo
+      return () => clearTimeout(timer);
+    }
+  }, [isAnimating, isCorrect]);
+
   const visibilityClass = isVisible ? '' : 'hidden';
 
   return (
-    <div className={`pokemon-card ${feedbackClass} ${visibilityClass}`} onClick={onClick}>
-      <img src={`/media/sprites/${pokemon.id}.png`} alt={pokemon.name} />
-      <p>{pokemon.name}</p>
+    <div 
+      className={`pokemon-card ${animationClass} ${visibilityClass}`} 
+      onClick={onClick}
+    >
+      <div className="card-inner">
+        <div className="card-front">
+          <img 
+            src={`/media/sprites/${pokemon.id}.png`} 
+            alt={pokemon.name} 
+            className="pokemon-image"
+          />
+          <p className="pokemon-name">{pokemon.name}</p>
+        </div>
+        <div className="card-back">
+          <div className="feedback-icon"></div>
+        </div>
+      </div>
     </div>
   );
 }
