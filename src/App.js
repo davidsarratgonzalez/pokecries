5889a6300607440f74b72eb6f47faa5642407d7f
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-08-29 13:44:24 +0200

    feat: add gradient overlay

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import StartScreen from './components/StartScreen';
 import BackgroundTransition from './components/BackgroundTransition';
+import GradientOverlay from './components/GradientOverlay';
 
 function App() {
   return (
     <div className="App">
       <BackgroundTransition />
+      <GradientOverlay />
       <StartScreen />
     </div>
   );
---
+++ src/components/GradientOverlay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './GradientOverlay.css';
+
+const GradientOverlay = () => {
+  return <div className="gradient-overlay" />;
+};
+
+export default GradientOverlay;
